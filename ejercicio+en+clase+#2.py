235c46860618dda98aef60c59285d5a80693bc8d
fausto andrade <ayf1207@gmail.com>
2016-08-27 08:02:33 -0500

    actualizar

---
+++ ejercicio+en+clase+#2.py
@@ -0,0 +1,53 @@
+
+# coding: utf-8
+
+# In[4]:
+
+np.e
+
+
+# In[5]:
+
+np.pi
+
+
+# In[7]:
+
+np.log(2)
+
+
+# In[8]:
+
+np.array([1,2,3])
+
+
+# In[9]:
+
+np.array([1.0,2.0,3.0])
+
+
+# In[10]:
+
+a=np.array([1,2,3])
+a=_8
+
+
+# In[11]:
+
+print (a.dtype)
+
+
+# In[17]:
+
+b= np.array([1,2,3],dtype= float)
+
+
+# In[18]:
+
+print (b)
+
+
+# In[ ]:
+
+
+
